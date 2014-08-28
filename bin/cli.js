6cffc99ea110cde4ac4fa8468bcabd58eca912a8
Leigh Zhu <i@zhuli.me>
2014-08-28 16:30:11 +0800

    adjust file structure && add cli.js

---
+++ bin/cli.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+var path = require('path');
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+var name = process.argv[2];
+
+var projDir = path.join(process.cwd(), name);
+
+var scaffold = path.join(path.dirname(__dirname), 'scaffold');
+
+var cpCmd = 'cp -rf ' + scaffold + ' ' + projDir;
+exec(cpCmd, function (err, stdout, stderr) {
+    if (err) throw err;
+
+    exec('git config --get user.name', function(err, stdout, stderr) {
+        if(err) throw err;
+        var author = stdout.replace(/[\n\r]/g, '');
+        var content = fs.readFileSync(projDir + '/package.json', 'utf8');
+        content = content.replace(/#name#/g, name);
+        content = content.replace(/#author#/, author);
+        fs.writeFileSync(projDir + '/package.json', content);
+
+        fs.mkdirSync(path.join(projDir, 'src'));
+        fs.mkdirSync(path.join(projDir, 'src/static'));
+        fs.mkdirSync(path.join(projDir, 'src/views'));
+    });
+});
